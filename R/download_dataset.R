9e7df32bd4d8d3f5ea2c969095fac4f3373d8203
Morteza Ansarinia <ansarinia@me.com>
2024-03-28 14:37:08 +0100

    cleanup for next cran release

--- R/download_dataset.R
+++ R/download_dataset.R
@@ -1,11 +1,3 @@
-#' Load a Behaverse dataset
-#' 
-#' @param name The name of the dataset to load
-#' @export 
-load_dataset <- function(name) {
-  paste0("Loading ", name, " dataset...")
-}
-
 #' Download a Behaverse dataset
 #' @param name The name of the dataset to download
 #' @param dest The destination directory to save the dataset
