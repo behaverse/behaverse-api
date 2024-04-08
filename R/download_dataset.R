978c9d274304d5fce84d889c833e726df97e0f72
Morteza Ansarinia <ansarinia@me.com>
2024-04-08 14:00:26 +0200

    Update download_dataset function to accept URL parameter and save the dataset to the specified file

--- R/download_dataset.R
+++ R/download_dataset.R
@@ -1,7 +1,20 @@
 #' Download a Behaverse dataset
-#' @param name The name of the dataset to download
-#' @param dest The destination directory to save the dataset
+#' @param url URL of the dataset to download
+#' @param dest The destination file to save the dataset
 #' @export
-download_dataset <- function(name, dest = NULL) {
-  paste0("Downloading ", name, " dataset...")
+download_dataset <- function(url, dest) {
+
+  parent_dir <- dirname(dest)
+
+  if (!dir.exists(parent_dir)) {
+    dir.create(parent_dir, recursive = TRUE)
+  }
+
+  if (!file.exists(dest)) {
+    download.file(url, dest)
+    print(paste("Dataset downloaded to", dest))
+  }
+
+  untar(dest, exdir = parent_dir)
+  print(paste("Dataset extracted to", parent_dir))
 }
--- R/load_dataset.R
+++ R/load_dataset.R
@@ -1,16 +1,7 @@
 #' Load a Behaverse dataset
-#' 
+#'
 #' @param name The name of the dataset to load
-#' @export 
+#' @export
 load_dataset <- function(name) {
   paste0("Loading ", name, " dataset...")
-}
-
-#' Download a Behaverse dataset
-#' 
-#' @param name The name of the dataset to download
-#' @param dest The destination directory to save the dataset
-#' @export
-download_dataset <- function(name, dest = NULL) {
-  paste0("Downloading ", name, " dataset...")
 }
