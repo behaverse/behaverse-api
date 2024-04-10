b2bed33fdf8ef741abbc07dc22685d3bdf44e589
Morteza Ansarinia <ansarinia@me.com>
2024-04-10 15:14:44 +0200

    improve docs

--- src/behaverse/__init__.py
+++ src/behaverse/__init__.py
@@ -1,3 +1,8 @@
 """Behaverse Python Package."""
 
 __version__ = '0.0.2-dev3'
+
+from .dataset import Dataset
+from .dataset_info import DatasetInfo
+
+__all__ = ['Dataset', 'DatasetInfo']
--- src/behaverse/dataset.py
+++ src/behaverse/dataset.py
@@ -10,33 +10,55 @@ logger = logging.getLogger(__name__)
 
 
 class Dataset():
-    """Dataset provides methods to download and load datasets."""
+    """Dataset provides methods to download and load datasets.
+
+    :::{.callout-note}
+
+    Notes:
+        You cannot manually instantiate this class. Use the class methods instead, e.g.
+        `Dataset.download()` or `Dataset.load()`. You can also pass additional
+        `allow_instantiation=True` to bypass this restriction.
+    :::
+
+    Args:
+        path (Path): the path to the dataset file.
+        kwargs (dict): additional arguments.
+
+    Raises:
+        NotImplementedError: you cannot instantiate this class. Use the class methods instead.
+    """
 
     def __init__(self, path: Path, **kwargs) -> None:
-        """Initialize the Dataset class.
-
-        Args:
-            path (Path): the path to the dataset file.
-            kwargs (dict): additional arguments.
-
-        Raises:
-            NotImplementedError: you cannot instantiate this class. Use the class methods instead.
-        """
+        """Initialize the Dataset class."""
         if not kwargs.get('allow_instantiation', False):
             raise NotImplementedError('You cannot instantiate this class. Use the class methods instead.')
 
         self.path = path
 
+        # TODO add dataset-level attributes
+        self.info = DatasetInfo()
+        self.subjects = ...
+        self.study_flow = ...
+        self.data = ...
+        self.response = ...
+        self.stimulus = ...
+        self.option = ...
+
     @classmethod
     def download(cls, url: str, dest: Path | str, **kwargs) -> Path:
-        """Download dataset from the given URL."""
+        """Download dataset from the given URL.
+
+        Args:
+            url: the URL to download the dataset from.
+            dest: the path to save the dataset file.
+            kwargs (dict): additional arguments.
+        """
         if not url or not dest:
             raise ValueError('URL and path are required.')
 
         chunk_size = kwargs.get('chunk_size', 8192)
 
-        if not isinstance(dest, Path):
-            # support string path
+        if isinstance(dest, str):
             dest = Path(dest)
 
         if dest.exists():
@@ -58,16 +80,19 @@ class Dataset():
         return output_path
 
     @classmethod
-    def load(cls, path: Path) -> 'Dataset':
+    def load(cls, path: Path | str) -> 'Dataset':
         """Load the dataset from the given path.
 
         Args:
-            path (Path): the path to the dataset file.
+            path: the path to the dataset file.
 
         Raises:
             FileNotFoundError: if the dataset directory does not exist.
 
         """
+        if isinstance(path, str):
+            path = Path(path)
+
         if not path.exists():
             raise FileNotFoundError(f'File not found: {path}')
 
@@ -76,9 +101,14 @@ class Dataset():
     def validate(self) -> bool:
         """Simple validations to check if the dataset is valid and consistent.
 
-        Violations will be logged as errors. Validation rules include:
-            - The dataset path should exist.
-            - TODO add more rules here.
+        :::{.callout-note}
+
+        Notes:
+            Violations will be logged as errors. Validation rules include:
+
+                1. The dataset path should exist.
+                2. TODO add more rules here.
+        :::
 
         Returns:
             bool: True if the dataset is valid, False otherwise.
@@ -91,7 +121,3 @@ class Dataset():
         except AssertionError as e:
             logger.error(e)
             return False
-
-    def info(self) -> DatasetInfo:
-        """Get dataset information."""
-        raise NotImplementedError('Get dataset information is not implemented yet.')
