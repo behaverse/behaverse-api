11fe8808a76d768136636376d1dc99812aea5a1e
Morteza Ansarinia <ansarinia@me.com>
2024-04-15 11:45:16 +0200

    new test url and fix the api

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -7,6 +7,6 @@ from behaverse.dataset import Dataset
 
 def test_download_dataset_from_onedrive():
     """Download a behaverse dataset from a public OneDrive link."""
-    url = ('https://uniluxembourg-my.sharepoint.com/:u:/g/personal/morteza_ansarinia_uni_lu/Ect90jSbAJBIrGNdbFqPbGUBCoH9zaeHoyvcpaKkZjSOqw?e=RUbZGh&download=1')
+    url = ('https://edu.lu/kwmqa')
 
-    Dataset.download_dataset(url, Path('tmp/P500-L1m.tar.gz'))
+    Dataset.download(url, Path('tmp/P500-L1m.tar.gz'))
